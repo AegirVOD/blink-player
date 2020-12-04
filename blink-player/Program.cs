b909adc01bdffaf6e07f074a41bb874371bb6390
AegirVOD <xiahouheiyin@gmail.com>
2020-12-04 21:36:46 +0100

    Creation of the project

---
+++ blink-player/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace blink_player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
